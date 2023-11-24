91b84812d01404d8de9df46a921ed66b33c7954d
Juan Diego <diego.mintic@gmail.com>
2023-11-24 11:17:17 -0500

    Formulación inicial del reto1

---
+++ reto1.py
@@ -0,0 +1,13 @@
+#Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales.
+#Desarrolle un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.
+
+min_age = 16
+income = 1000
+
+user_age = int(input('Digite la edad: '))
+user_income = float(input('Digite los ingresos mensuales: '))
+
+if user_age > min_age and user_income >= income:
+    print(f'El usuario con: {user_age} años e ingresos mensuales iguales o superiores a: ${user_income}, DEBE tributar')
+else:
+    print('El usuario NO DEBE tributar')
